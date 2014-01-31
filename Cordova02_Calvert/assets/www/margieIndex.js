f4b0917bb5cf2699961829e0e4bf6a28ceb69c06
Margie Calvert <margiecalvert@gmail.com>
2014-01-31 15:16:51 -0800

    This has all the unit tests, Converter assignment

--- Cordova02_Calvert/assets/www/margieIndex.js
+++ Cordova02_Calvert/assets/www/margieIndex.js
@@ -5,20 +5,20 @@
 var Clicker = (function() {
 
 	var that = null;
-	
+
 	function Clicker() {
 		that = this;
 		$("#button01").click(tempConvertPrivate);
-		$("#button02").click(this.milesConvert);
-		$("#button03").click(this.sqrConvert);
+		$("#button02").click(milesConvertPrivate);
+		$("#button03").click(sqrConvertPrivate);
 	};
-	
-	var tempConvertPrivate = function(){
+
+	var tempConvertPrivate = function() {
 		var inputData = $('#tempInputData').val();
 		that.tempConvert(inputData);
 	};
 
-	 Clicker.prototype.tempConvert = function(inputData) {
+	Clicker.prototype.tempConvert = function(inputData) {
 		var outputData1 = (inputData - 32);
 		var outputData2 = outputData1 * 5;
 		var outputData3 = outputData2 / 9;
@@ -30,24 +30,33 @@ var Clicker = (function() {
 		return outputData4;
 	};
 
-	Clicker.prototype.milesConvert = function() {
-
+	var milesConvertPrivate = function() {
 		var inputData = $('#milesInputData').val();
+		that.milesConvert(inputData);
+	};
+
+	Clicker.prototype.milesConvert = function(inputData) {
 		var outputData = (1.60934 * inputData);
 		var stringToShowUser = 'Kilometers: ' + outputData;
 		var inputStringToShowUser = 'Miles: ' + inputData;
 		$('#kmOutputData').val(stringToShowUser);
 		$('#milesInputData').val(inputStringToShowUser);
+		outputDataNum = parseInt(outputData);
+		return outputDataNum;
 	};
 
-	Clicker.prototype.sqrConvert = function() {
-
+	var sqrConvertPrivate = function() {
 		var inputData = $('#sqrInputData').val();
+		that.sqrConvert(inputData);
+	};
+
+	Clicker.prototype.sqrConvert = function(inputData) {
 		var outputData = (Math.sqrt(inputData));
 		var stringToShowUser = 'Square Root: ' + outputData;
 		var inputStringToShowUser = 'Integer: ' + inputData;
 		$('#sqrOutputData').val(stringToShowUser);
 		$('#sqrInputData').val(inputStringToShowUser);
+		sqrOutputDataNum = parseInt(outputData);
 	};
 	return Clicker;
 
--- Cordova02_Calvert/assets/www/test.js
+++ Cordova02_Calvert/assets/www/test.js
@@ -1,20 +1,20 @@
 describe("Converter", function() {
-  it("Confirm that tempConverter works", function() {
-  	var clicker = new Clicker();
-  	var actual = clicker.tempConvert(59);
-    expect(actual).toBe(15);
-  });
-  
-  it("One plus one equals two", function() {
-    expect(1+1).toBe(2);
-  });
-  
-  it("Call addMe() with 2 + 3 = 5", function() {
-    expect(addMe()).toBe(5);
-  });
-  
-  it("Call multiplyMe() with 2 * 3 = 6", function() {
-    expect(multiplyMe(2, 3)).toBe(6);
-  });
-  
-});
+	it("Confirm that tempConverter works", function() {
+		var clicker = new Clicker();
+		var actual = clicker.tempConvert(59);
+		expect(actual).toBe(15);
+	});
+
+	it("Confirm that MilesConverter works", function() {
+		var clicker = new Clicker();
+		var actual = clicker.milesConvert(150);
+		expect(actual).toBe(241);
+	});
+
+	it("Confirm that sqrConverter works", function() {
+		var clicker = new Clicker();
+		var actual = clicker.sqrConvert(16);
+		expect(actual).toBe(4);
+	});
+
+}); 
