bae802179ce56810d8a2b20d93e1a027287906b4
margiecalvert <margiecalvert@gmail.com>
2014-03-27 13:13:41 -0700

    final with Options.json working on two fields, sort of

---
+++ Prog272Final_Calvert/AwsBasicS3/Source/AwsUi.js
@@ -0,0 +1,174 @@
+define(['jquery'], function() {'use strict';
+
+    var buttons = null;
+    var options = null;
+    var transformOptions = null;
+    var dataIndex = 0;
+    var dataIndexTransform = 0;
+    var changeCount = 0;
+
+    function AwsUi() {
+        $("#listBuckets").click(listBuckets);
+        $("#copyToS3").click(copyToS3);
+        $("#getOptions").click(getOptions);
+        $("#transformForwardButton").click(forwardTransform);
+        $("#tranformBackButton").click(backwardTransform);
+        $("#forwardButton").click(forward);
+        $("#backButton").click(backward);
+        $("#changeOptions").click(saveOptions);
+
+        $("#buildAll").click(buildAll);
+        getBuildConfig();
+        getOptions();
+    }
+    
+
+
+    var buildAll = function() {
+        $.getJSON("/buildAll", {
+            options : JSON.stringify(transformOptions),
+            index : dataIndexTransform
+        }, function(result) {
+            $("#buildData").empty();
+            var fileArray = result.data.split("\n");
+            for (var i = 0; i < fileArray.length; i++) {
+                if (fileArray[i].length > 0) {
+                    $("#buildData").append("<li>" + fileArray[i] + "</li>");
+                }
+            }
+        });
+    };
+    
+    var saveOptions = function() {
+		var pathToConfig = $("#pathToConfig").val();
+        options[dataIndex].pathToConfig = pathToConfig;
+        var reallyWrite = $("#reallyWrite").val();
+        options[dataIndex].reallyWrite = reallyWrite;
+        $.getJSON("/saveOptions", {
+            options : JSON.stringify(options)
+        }, function(data) {
+			$("#changeResult").html(data.result + changeCount++);			
+		    $("#pathToConfig").val = options[dataIndex].pathToConfig;
+		    $("#reallyWrite").val = options[dataIndex].reallyWrite;		 		    
+		});
+    };
+    
+
+    var copyToS3 = function() {
+        $.getJSON("/copyToS3", {
+            options : JSON.stringify(options[dataIndex])
+        }, function(data) {
+            $("#copyResult").html("Result: " + data.result);
+        });
+    };
+
+    var displayTransformConfig = function(options) {
+        $("#pathToPython").val(options.pathToPython);
+        $("#copyFrom").val(options.copyFrom);
+        $("#copyTo").val(options.copyTo);
+        $("#filesToCopy").val(options.filesToCopy);
+    };
+
+    var displayOptions = function(options) {
+		
+        $("#currentDocument").val(dataIndex + 1);
+        $("#pathToConfig").val(options.pathToConfig);
+        $("#reallyWrite").val(options.reallyWrite ? "true" : "false");
+        $("#bucketName").val(options.bucketName);
+        $("#folderToWalk").val(options.folderToWalk);
+        $("#s3RootFolder").val(options.s3RootFolder);
+        $("#createFolderToWalkOnS3").val(options.createFolderToWalkOnS3 ? "true" : "false");
+        $("#createIndex").val(options.createIndex ? "true" : "false");
+        $("#filesToIgnore").val(options.filesToIgnore);
+    };
+
+    var getBuildConfig = function() {
+        $.getJSON("/getBuildConfig", function(optionsInit) {
+            transformOptions = optionsInit;
+            displayTransformConfig(transformOptions[dataIndexTransform]);
+        }).fail(function(a) {
+            alert(JSON.stringify(a));
+        });
+    };
+    var getOptions = function() {
+        $.getJSON("/getOptions", function(optionsInit) {
+            options = optionsInit;
+            $('#documentCount').val(options.length);
+            displayOptions(options[0]);
+        }).fail(function(a) {
+            alert(JSON.stringify(a));
+        });
+    };
+
+    var forwardTransform = function() {
+        if (dataIndexTransform < transformOptions.length - 1) {
+            dataIndexTransform++;
+            displayTransformConfig(transformOptions[dataIndexTransform]);
+        }
+    };
+
+    var backwardTransform = function() {
+        if (dataIndexTransform > 0) {
+            dataIndexTransform--;
+            displayTransformConfig(transformOptions[dataIndexTransform]);
+            return dataIndexTransform;
+        }
+        return dataIndexTransform;
+    };
+    
+    //my borrowing from DatInput01
+    function getUserNumber() {
+		
+		
+		return $("#userNumber").val();
+	}
+    //Charlie's code
+
+	var updateInputs = function() {
+		var pathToConfig = $("#pathToConfig").val();
+		options.pathToConfig = pathToConfig;
+		return options.pathToConfig;
+	}	
+		
+	/*	var reallyWrite = $("#reallyWrite").val();
+		options.reallyWrite = reallyWrite;
+		
+		var bucketName = $("#bucketName").val();
+		options.bucketName = bucketName;
+		var folderToWalk = $("#folderToWalk").val();
+		var s3RootFolder = $("#s3RootFolder").val();
+		var createFolderToWalkOnS3 = $("#createFolderToWalkOnS3").val();
+		var createIndex = $("#createIndex").val();
+		var filesToIgnore = $("#filesToIgnore").val();
+		displayOptions(options);
+	} */
+	
+    var forward = function() {
+        if (dataIndex < options.length - 1) {
+            dataIndex++;
+            displayOptions(options[dataIndex]);
+            updateInputs();
+        }
+    };
+
+    var backward = function() {
+        if (dataIndex > 0) {
+            dataIndex--;
+            displayOptions(options[dataIndex]);
+            return true;
+        }
+        return false;
+    };
+
+    var listBuckets = function() {
+        $.getJSON("/listBuckets", {
+            options : JSON.stringify(options[dataIndex])
+        }, function(data) {
+            for (var i = 0; i < data.length; i++) {
+                $("#buckets").append("<li>" + data[i] + "</li>");
+            }
+        });
+    };
+
+    return AwsUi;
+});
---
+++ Prog272Final_Calvert/AwsBasicS3/app.js
@@ -0,0 +1,154 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+//var routes = require('./routes');
+//var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var walkDirs = require("./Source/WalkDirs").walkDirs;
+var s3Code = require("./Source/S3Code");
+var fs = require("fs");
+var exec = require('child_process').exec;
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 30025);
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Source')));
+app.use(express.static(path.join(__dirname, 'Images')));
+app.use(express.favicon('Images/favicon16.ico'));
+// app.use(express.favicon(path.join(__dirname, 'favicon16.ico')));
+
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
+
+app.get('/', function(request, response) { 'use strict';
+    var html = fs.readFileSync(__dirname + '/public/index.html');
+    response.writeHeader(200, {"Content-Type": "text/html"});   
+    response.write(html);
+    response.end();
+});
+
+//app.get('/', routes.index);
+// app.get('/users', user.list);
+
+/*
+ * You will need to edit one or more objects in Options.json. 
+ * They have this general format
+
+var options = {
+		pathToConfig: '/home/charlie/config.json',		
+		reallyWrite: true, 
+		bucketName: 'bucket01.elvenware.com',
+		folderToWalk: "Files",
+		s3RootFolder: "FilesTwo",
+		createFolderToWalkOnS3: true,
+		createIndex: true,
+		filesToIgnore: ['Thumbs.db', '.gitignore', 'MyFile.html']
+};
+ 
+ * Before filling it out, see the README file for this project. 
+ */	
+
+app.get('/getOptions', function(request, response) {'use strict';
+	var options = fs.readFileSync("Options.json", 'utf8');
+	console.log(typeof options);
+	console.log(options);
+	options = JSON.parse(options);
+	response.send(options);
+});
+
+app.get('/listBuckets', function(request, response) {'use strict';
+    console.log("ListBuckets called");
+    console.log(request.query);
+	var options = JSON.parse(request.query.options);
+	console.log("ListBuckets: ", options.pathToConfig);
+	s3Code.loadConfig(options.pathToConfig);
+	s3Code.listBuckets(response, true);
+});
+
+app.get('/copyToS3', function(request, response) {'use strict';
+	console.log(typeof request.query.options);	
+	var options = JSON.parse(request.query.options);
+	console.log(options);
+	walkDirs(options, response);
+});
+
+app.get('/saveOptions', function(request, response) {'use strict';
+	console.log("saveOptions called");
+	var options = request.query.options;
+	console.log(options);
+	console.log(typeof options);
+	// This is just to create pretty format, the next two lines can be deleted.
+	options = JSON.parse(options);
+	options = JSON.stringify(options, null, 4);
+	//var data = JSON.parse(request.query.options);
+	// console.log("Saved Options are: " + options);
+	fs.writeFile("Options.json", options, "utf8", function(err, data) {			
+		if (err) {
+			console.log("Errror");
+			response.send({"result": "Error", "details": err});
+		}
+		console.log("FileWritten");
+		response.send({"result": "Success", data: data});
+	});
+});
+
+/*var writeFile = function(response, jsonString) {
+		fs.writeFile("test.md", jsonString, function(err) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log("The file was saved!");
+				convertToHtml(response);
+			}			
+		});
+	}; */
+
+var buildAll = function(response, config, index) { 'use strict';
+	console.log("BuildAll was called");
+	// var config = fs.readFileSync("MarkdownTransformConfig.json", 'utf8');	
+	// config = JSON.parse(config);
+	var command = config[index].pathToPython + " MarkdownTransform.py -i " + index;	
+	try {
+		exec(command, function callback(error, stdout, stderr) {
+			// Read in the HTML send the HTML to the client
+			console.log("convertToHtml was called er: ", error);
+			console.log("convertToHtml was called so: ", stdout);
+			console.log("convertToHtml was called se: ", stderr);
+			response.send({ "result": "Success", "data": stdout });
+		});
+	} catch(e) {
+		console.log(e.message);
+		response.send( { "result" : "error", "data": e });
+	}
+};
+
+app.get('/buildAll', function(request, response) { 'use strict';
+	console.log("buildAll called");	
+	var options = JSON.parse(request.query.options);
+	buildAll(response, options, request.query.index);
+});
+
+app.get('/getBuildConfig', function(request, response) { 'use strict';
+	console.log('getBuildConfig called');
+	var options = fs.readFileSync("MarkdownTransformConfig.json", 'utf8');
+	options = JSON.parse(options);
+	response.send(options);
+});
+
+http.createServer(app).listen(app.get('port'), function() {'use strict';
+	console.log('Express server listening on port ' + app.get('port'));
+});
